24918cdf290ec438ed01dfdda9d38d041afafb36
IsomerX <49413460+IsomerX@users.noreply.github.com>
2023-11-12 19:31:51 +0530

    added the footer and stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Cards from "~/components/Cards";
+import Footer from "~/components/Footer/Footer";
 
 export default function Home() {
     return (
         <div>
             <div className="heading">MAGIC CARDS</div>
+            <div className="subheading">(hover to start)</div>
             <Cards />
+            <Footer />
         </div>
     );
 }
